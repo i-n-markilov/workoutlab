36e0232c8a495cb8352519429885d1727964a6b9
i-n-markilov <markilov1@gmail.com>
2026-02-21 14:21:48 +0200

    updated home page

--- common/views.py
+++ common/views.py
@@ -18,24 +18,3 @@ def home(request: HttpRequest) -> HttpResponse:
 
     return render(request, 'common/home-page.html', context)
 
-
-# def last_equipment_added(request: HttpRequest) -> HttpResponse:
-#     last_equipment = Equipment.objects.all().order_by('-created').first()
-#
-#     context = {'last_equipment': last_equipment}
-#
-#     return render(request, 'common/home-page.html', context)
-#
-# def last_exercise_added(request: HttpRequest) -> HttpResponse:
-#     last_exercise = Exercise.objects.all().order_by('-created').first()
-#
-#     context = {'last_exercise': last_exercise}
-#
-#     return render(request, 'common/home-page.html', context)
-#
-# def last_workout_plan_created(request: HttpRequest) -> HttpResponse:
-#     last_workout_plan = WorkoutPlan.objects.all().order_by('-created').first()
-#
-#     context = {'last_workout_plan': last_workout_plan}
-#
-#     return render(request, 'common/home-page.html', context)
