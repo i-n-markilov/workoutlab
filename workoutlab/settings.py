c9a04db68f28321f0780898e54b978c3c0a9d23f
i-n-markilov <markilov1@gmail.com>
2026-04-04 00:55:06 +0300

    chore: updated yaml

--- workoutlab/settings.py
+++ workoutlab/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [host for host in os.getenv('ALLOWED_HOSTS').split(',') if host]
+ALLOWED_HOSTS = [host for host in os.getenv('ALLOWED_HOSTS', '').split(',') if host]
 
 
 
