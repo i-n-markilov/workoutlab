5182947a30931f0156dd81cbee8a53876dd24481
i-n-markilov <markilov1@gmail.com>
2026-04-06 23:21:02 +0300

    chore:removed celery setup due to issues with redis cache on azure

--- workoutlab/__init__.py
+++ workoutlab/__init__.py
@@ -1,3 +1,6 @@
-from .celery import app as celery_app
+# from .celery import app as celery_app
+#
+# __all__ = ('celery_app',)
 
-__all__ = ('celery_app',)
+
+#issues with setting redis cache on azure. webjob can't run
